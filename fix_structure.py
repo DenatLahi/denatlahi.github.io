a10a6957c4249ad424800dcd2655534eeff0f275
DenatLahi <142692935+DenatLahi@users.noreply.github.com>
2025-04-15 00:20:31 +0200

    v

--- fix_structure.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-for file in os.listdir():
-    if file.endswith(".html") and file != "index.html":
-        name = file[:-10] if file.endswith("index.html") else file[:-5]
-        os.mkdir(name)
-        shutil.move(file, os.path.join(name, "index.html"))
